c249f23f2aa918f6c484b4d21c7439c57e350334
Conny Nikolaisen <conny@conik.net>
2026-01-27 22:35:24 +0100

    Startup files

---
+++ README.md
@@ -0,0 +1,116 @@
+This is a ABRP livedata client running on the Freematic ONE+ Model B.
+Made by https://github.com/spot2000
+
+Hardware do you find here:
+* Freematics ONE+ [Model B] (https://freematics.com/products/freematics-one-plus-model-b/) (mainly supported)
+* Freematics ONE+ [Model A] (https://freematics.com/products/freematics-one-plus/) (should work)
+* Freematics ONE+ [Model H] (https://freematics.com/products/freematics-one-plus-model-h/) (should work)
+
+
+You can find the latest version here:
+https://github.com/spot2000/Freematics-ABRP
+
+Freematic ONE+ Model B hardware info:
+-------------------------------------
+Features
+--------
+Dual-core Arduino programmable SoC with built-in WiFi and Bluetooth
+Access to all standard OBD-II PIDs, DTC, VIN from vehicle ECU
+CAN bus data sniffing
+High update rate and accuracy GNSS geolocation
+G-force measurement and motion detection
+Car battery voltage reading
+Massive data storage (microSD up to 32GB)
+Real-time data transmission over WiFi and cellular network
+Configuring and monitoring from mobile device app via BLE
+
+Specifications
+--------------
+Espressif ESP32 with 16MB Flash, 8MB PSRAM, 32K RTC
+Built-in 802.11 b/g/n Wi-Fi and dual mode Bluetooth (classic and BLE)
+Integrated ICM-42627 motion sensor
+Integrated u-blox M10 GNSS module and antenna
+Integrated SIM7070G global LTE-M cellular module
+Integrated buzzer
+Enclosure dimensions: 60x48x20mm
+
+Physical Interfaces
+-------------------
+OBD-II male connector
+microUSB port
+microSD card slot
+SIM card slot
+GPIO socket (Molex)
+
+Data Collection
+---------------
+
+This project collects the following data:
+-----------------------------------------
+* Geolocation data (from internal or external GNSS) 
+* Accelerometer and gyroscope data (from internal MEMS motion sensor)
+* Cellular or WiFi network signal level
+* Device temperature
+* EV vehicle data via OBD port (See ABRP data list below)
+
+List of ABRP data that is collected:
+------------------------------------
+
+High priority parameters:
+-------------------------
+utc [s]: Current UTC timestamp (epoch) in seconds (note, not milliseconds!)
+soc [SoC %]: State of Charge of the vehicle (what's displayed on the dashboard of the vehicle is preferred)
+power [kW]: Instantaneous power output/input to the vehicle. Power output is positive, power input is negative (charging)
+speed [km/h]: Vehicle speed
+lat [°]: Current vehicle latitude
+lon [°]: Current vehicle longitude
+is_charging [bool or 1/0]: Determines vehicle state. 0 is not charging, 1 is charging
+is_dcfc [bool or 1/0]: If is_charging, indicate if this is DC fast charging
+is_parked [bool or 1/0]: If the vehicle gear is in P (or the driver has left the car)
+
+The lower priority parameters (and expected units) are:
+-------------------------------------------------------
+capacity [kWh]: Estimated usable battery capacity (can be given together with soh, but usually not)
+kwh_charged [kWh]: Measured energy input while charging. Typically a cumulative total, but also supports individual sessions.
+soh [%]: State of Health of the battery. 100 = no degradation
+heading [°]: Current heading of the vehicle. This will take priority over phone heading, so don't include if not accurate.
+elevation [m]: Vehicle's current elevation. If not given, will be looked up from location (but may miss 3D structures)
+ext_temp [°C]: Outside temperature measured by the vehicle
+batt_temp [°C]: Battery temperature
+voltage [V]: Battery pack voltage
+current [A]: Battery pack current (similar to power: output is positive, input (charging) is negative.)
+odometer [km]: Current odometer reading in km.
+est_battery_range [km]: Estimated remaining range of the vehicle (according to the vehicle)
+
+Data storage:
+-------------
+Collected data are stored in a circular buffer in ESP32's IRAM or PSRAM. When PSRAM is enabled, hours of data can be buffered in case of temporary network outage and transmitted when network connection resumes.
+
+Configuration handling:
+-----------------------
+Configuration is done in the config.cfg file that is located in the config folder of the SD-card.
+See config file for more information.
+
+
+Data Transmission
+-----------------
+
+Data transmission over UDP and HTTP(s) protocols are implemented for the followings.
+
+* WiFi (ESP32 built-in)
+* 3G WCDMA (SIM5360)
+* 4G LTE CAT-4 (SIM7600)
+* 4G LTE CAT-M (SIM7070)
+
+Seamless WiFi and cellular network co-working is implemented. When defined WiFi hotspot is available, data is transmitted via WiFi and cellular module is switched off. When no WiFi hotspot can be reached, cellular module is switched on for data transmission until WiFi hotspot available again.
+
+Data Storage
+------------
+Following types of data storage are supported.
+
+* MicroSD card storage
+* ESP32 built-in Flash memory storage (SPIFFS)
+
+BLE & App
+---------
+Planned support in the future for ability to link phone to Freematic adapter to view data in a easy way. (not ready yet)
---
+++ config.h
@@ -0,0 +1,190 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#ifdef CONFIG_ENABLE_OBD
+#define ENABLE_OBD CONFIG_ENABLE_OBD
+#endif
+#ifdef CONFIG_ENABLE_MEMS
+#define ENABLE_MEMS CONFIG_ENABLE_MEMS
+#endif
+#ifdef CONFIG_GNSS
+#define GNSS CONFIG_GNSS
+#endif
+#ifdef CONFIG_STORAGE
+#define STORAGE CONFIG_STORAGE
+#endif
+#ifdef CONFIG_BOARD_HAS_PSRAM
+#define BOARD_HAS_PSRAM 1
+#endif
+#ifdef CONFIG_ENABLE_WIFI
+#define ENABLE_WIFI CONFIG_ENABLE_WIFI
+#define WIFI_SSID CONFIG_WIFI_SSID
+#define WIFI_PASSWORD CONFIG_WIFI_PASSWORD
+#endif
+#ifdef CONFIG_ENABLE_BLE
+#define ENABLE_BLE CONFIG_ENABLE_BLE
+#endif
+#ifdef CONFIG_ENABLE_HTTPD
+#define ENABLE_HTTPD CONFIG_ENABLE_HTTPD
+#endif
+#ifdef CONFIG_SERVER_HOST
+#define SERVER_HOST CONFIG_SERVER_HOST
+#define SERVER_PORT CONFIG_SERVER_PORT
+#define SERVER_PROTOCOL CONFIG_SERVER_PROTOCOL
+#endif
+#ifdef CONFIG_CELL_APN
+#define CELL_APN CONFIG_CELL_APN
+#endif
+
+/**************************************
+* Circular Buffer Configuration
+**************************************/
+#if BOARD_HAS_PSRAM
+#define BUFFER_SLOTS 1024 /* max number of buffer slots */
+#define BUFFER_LENGTH 384 /* bytes per slot */
+#define SERIALIZE_BUFFER_SIZE 4096 /* bytes */
+#else
+#define BUFFER_SLOTS 32 /* max number of buffer slots */
+#define BUFFER_LENGTH 256 /* bytes per slot */
+#define SERIALIZE_BUFFER_SIZE 1024 /* bytes */
+#endif
+
+/**************************************
+* Configuration Definitions
+**************************************/
+#define STORAGE_NONE 0
+#define STORAGE_SPIFFS 1
+#define STORAGE_SD 2
+
+#define GNSS_NONE 0
+#define GNSS_STANDALONE 1
+#define GNSS_CELLULAR 2
+
+#define PROTOCOL_UDP 1
+#define PROTOCOL_HTTPS_GET 2
+#define PROTOCOL_HTTPS_POST 3
+
+/**************************************
+* OBD-II configurations
+**************************************/
+#ifndef ENABLE_OBD
+#define ENABLE_OBD 1
+#endif
+
+// maximum consecutive OBD access errors before entering standby
+#define MAX_OBD_ERRORS 3
+
+/**************************************
+* Networking configurations
+**************************************/
+#ifndef ENABLE_WIFI
+#define ENABLE_WIFI 1
+// WiFi settings
+#define WIFI_SSID "skynet-2.4"
+#define WIFI_PASSWORD "waldemar72dennis"
+#endif 
+
+#ifndef SERVER_HOST
+// cellular network settings
+#define CELL_APN ""
+// Freematics Hub server settings
+#define SERVER_HOST "hub.freematics.com"
+#define SERVER_PROTOCOL PROTOCOL_UDP
+#endif
+
+// SIM card setting
+#define SIM_CARD_PIN ""
+#define APN_USERNAME NULL
+#define APN_PASSWORD NULL
+
+// HTTPS settings
+#define SERVER_PATH "/hub/api"
+
+#if !SERVER_PORT
+#undef SERVER_PORT
+#if SERVER_PROTOCOL == PROTOCOL_UDP
+#define SERVER_PORT 8081
+#else
+#define SERVER_PORT 443
+#endif
+#endif
+
+// WiFi Mesh settings
+#define WIFI_MESH_ID "123456"
+#define WIFI_MESH_CHANNEL 13
+
+// WiFi AP settings
+#define WIFI_AP_SSID "TELELOGGER"
+#define WIFI_AP_PASSWORD "PASSWORD"
+
+// maximum consecutive communication errors before resetting network
+#define MAX_CONN_ERRORS_RECONNECT 5
+// maximum allowed connecting time
+#define MAX_CONN_TIME 10000 /* ms */
+// data receiving timeout
+#define DATA_RECEIVING_TIMEOUT 5000 /* ms */
+// expected maximum server sync signal interval
+#define SERVER_SYNC_INTERVAL 120 /* seconds, 0 to disable */
+// data interval settings
+#define STATIONARY_TIME_TABLE {10, 60, 180} /* seconds */
+#define DATA_INTERVAL_TABLE {1000, 2000, 5000} /* ms */
+#define PING_BACK_INTERVAL 900 /* seconds */
+#define SIGNAL_CHECK_INTERVAL 10 /* seconds */
+
+/**************************************
+* Data storage configurations
+**************************************/
+#ifndef STORAGE
+// change the following line to change storage type
+#define STORAGE STORAGE_SD
+#endif
+
+/**************************************
+* MEMS sensors
+**************************************/
+#ifndef ENABLE_MEMS
+#define ENABLE_MEMS 1
+#endif
+
+/**************************************
+* GPS
+**************************************/
+#ifndef GNSS
+// change the following line to change GNSS setting
+#define GNSS GNSS_STANDALONE
+#endif
+// keeping GNSS power on during standby 
+#define GNSS_ALWAYS_ON 0
+// GNSS reset timeout while no signal
+#define GNSS_RESET_TIMEOUT 300 /* seconds */
+
+/**************************************
+* Standby/wakeup
+**************************************/
+// motion threshold for waking up
+#define MOTION_THRESHOLD 0.4f /* vehicle motion threshold in G */
+// engine jumpstart voltage for waking up (when MEMS unavailable) 
+#define JUMPSTART_VOLTAGE 14 /* V */
+// reset device after waking up
+#define RESET_AFTER_WAKEUP 1
+
+/**************************************
+* Additional features
+**************************************/
+#define PIN_SENSOR1 34
+#define PIN_SENSOR2 26
+
+#define COOLING_DOWN_TEMP 75 /* celsius degrees */
+
+// enable(1)/disable(0) http server
+#ifndef ENABLE_HTTPD
+#define ENABLE_HTTPD 0
+#endif
+
+// enable(1)/disable(0) BLE SPP server (for Freematics Controller App).
+#ifndef ENABLE_BLE
+#define ENABLE_BLE 1
+#endif
+
+
+#endif // CONFIG_H_INCLUDED
